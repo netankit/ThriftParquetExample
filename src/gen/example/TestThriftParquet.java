7d0b38bfd31269f2cedae503b17300f2cac16112
netankit <ankit.bahuguna@cs.tum.edu>
2014-04-23 20:26:12 +0200

    Added a use case code segment to TestThiftParquet.java

--- src/gen/example/TestThriftParquet.java
+++ src/gen/example/TestThriftParquet.java
@@ -1,54 +1,61 @@
 package gen.example;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.Arrays;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.TaskAttemptID;
 import org.apache.thrift.TBase;
-import org.junit.Test;
+import org.apache.thrift.TException;
+import org.apache.thrift.TSerializer;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.protocol.TProtocolFactory;
 
-import parquet.Log;
-import parquet.example.data.Group;
-import parquet.hadoop.ParquetReader;
-import parquet.hadoop.thrift.ParquetThriftBytesOutputFormat;
-import parquet.hadoop.thrift.ThriftToParquetFileWriter;
-import parquet.hadoop.util.ContextUtil;
-import parquet.org.apache.thrift.TException;
-import parquet.org.apache.thrift.protocol.TCompactProtocol;
-import parquet.org.apache.thrift.protocol.TProtocol;
-import parquet.org.apache.thrift.protocol.TProtocolFactory;
-import parquet.org.apache.thrift.transport.TIOStreamTransport;
-
-import com.twitter.data.proto.tutorial.thrift.AddressBook;
-import com.twitter.data.proto.tutorial.thrift.Name;
-import com.twitter.data.proto.tutorial.thrift.Person;
-import com.twitter.data.proto.tutorial.thrift.PhoneNumber;
+import parquet.hadoop.metadata.CompressionCodecName;
+import parquet.thrift.ThriftParquetWriter;
 
 public class TestThriftParquet {
-	
-	Path myfileToCreate;
-	Configuration conf = new Configuration();
-	TaskAttemptID taskId = new TaskAttemptID("local", 0, true, 0, 0);
+	public void myTestImplementation() throws IOException,
+			InterruptedException, TException {
+		// Creating object for thrift generated java file
+		Employee emp = new Employee();
 
-	TaskAttemptContext mytaskAttemptContext = ContextUtil
-			.newTaskAttemptContext(conf, taskId);
-	TProtocolFactory myprotocolFactory = new TCompactProtocol.Factory();
+		// Add the values to the above object
+		emp.setId("1");
+		emp.setName("Ankit");
+		emp.setAddress("Munich, Deutchland");
+		emp.setPhoneNumber("11111111");
 
-	Class<? extends TBase<?, ?>> mythriftClass = Employee.class;	
-	
-	ThriftToParquetFileWriter testObj = new ThriftToParquetFileWriter(
-		myfileToCreate, mytaskAttemptContext, myprotocolFactory,
-			mythriftClass);
+		// To send this across different platforms like DOTNET etc...
+		// this object needs to be serialized
+		// The serialized thrift object is nothing but a byte array
 
-			ParquetThriftBytesOutputFormat ptb = new ParquetThriftBytesOutputFormat();
+		byte[] empDtl = null;
+		TSerializer serializer = new TSerializer();
+		try {
+			empDtl = serializer.serialize(emp);
+		} catch (TException e) {
+			e.printStackTrace();
+		}
 
-		 
+		Path fileToCreate = new Path("target/emp.parquet");
+		TProtocolFactory protocolFactory = new TCompactProtocol.Factory();
+		Configuration conf = new Configuration();
+		TaskAttemptID taskId = new TaskAttemptID("local", 0, true, 0, 0);
+
+		ThriftParquetWriter<TBase<?, ?>> w2 = new ThriftParquetWriter<TBase<?, ?>>(
+				fileToCreate, (Class<TBase<?, ?>>) emp.getClass(),
+				CompressionCodecName.UNCOMPRESSED);
+
+		// w2.write(); // Need to add a Thrift object of type TBase.
+		w2.close();
+
+		FileSystem fileSystem = fileToCreate.getFileSystem(conf);
+		boolean exists = fileSystem.exists(fileToCreate);
+		assertEquals(exists, true);
 	}
+
 }
